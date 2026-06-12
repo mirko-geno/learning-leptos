da45c51a02babfc110b9a86a66c0daa47603cbb9
mirko-geno <mirkogenovesi24@gmail.com>
2026-06-12 02:47:30 -0300

    even better solution

--- dist/leptos-tutorial-e15186516e075fee.js
+++ dist/leptos-tutorial-e15186516e075fee.js
@@ -389,17 +389,17 @@ function __wbg_get_imports() {
             console.warn(arg0, arg1, arg2);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 108, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h0a7454e584d83a84);
-            return ret;
-        },
-        __wbindgen_cast_0000000000000002: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 189, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 195, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h7b3693b488d542aa);
             return ret;
         },
+        __wbindgen_cast_0000000000000002: function(arg0, arg1) {
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 95, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h0a7454e584d83a84);
+            return ret;
+        },
         __wbindgen_cast_0000000000000003: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("Event")], shim_idx: 109, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("Event")], shim_idx: 96, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__he17af71671e611f5);
             return ret;
         },
--- src/bin/rusty_sintax.rs
+++ src/bin/rusty_sintax.rs
@@ -3,46 +3,53 @@ use leptos::{
     html::{div, button, span, p},
     prelude::*,
 };
+use reactive_stores::Store;
 
-pub fn counter(
-    rvalue: ReadSignal<i32>, wvalue: WriteSignal<i32>,
-    rlock: ReadSignal<bool>, wlock: WriteSignal<bool>,
-    initial_value: i32, step: i32
-) -> impl IntoView {
-    wvalue.set(initial_value);
 
+#[derive(Store)]
+pub struct ControlledCounter {
+    value: i32,
+    lock: bool
+}
+impl Default for ControlledCounter {
+    fn default() -> Self {
+        Self { value: 0, lock: false }
+    }
+}
+
+pub fn counter(store_counter: Store<ControlledCounter>, step: i32) -> impl IntoView {
     div().child((
         button()
             .on(ev::click, move |_| {
-                wvalue.set(0);
-                wlock.set(false);
+                store_counter.value().set(0);
+                store_counter.lock().set(false);
             })
             .child("Clear"),
 
         button()
             .on(ev::click, move |_| {
-                let result = rvalue.get() - step;
-                match rlock.get() {
-                    true => if result > 5 { *wvalue.write() = result},
-                    false => *wvalue.write() = result
+                let result = store_counter.value().get() - step;
+                match store_counter.lock().get() {
+                    true => if result > 5 { store_counter.value().set(result)},
+                    false => store_counter.value().set(result)
                 }
             })
             .child("-1"),
 
         button()
             .on(ev::click, move |_| {
-                *wvalue.write() += step;
-                if rvalue.get() > 5 { *wlock.write() = true };
+                *store_counter.value().write() += step;
+                if store_counter.value().get() > 5 { store_counter.lock().set(true) };
             })
             .child("+1"),
         
-        span().child(("Value: ", move || rvalue.get(), "!")),
+        span().child(("Value: ", move || store_counter.value().get(), "!")),
     ))
 }
 
-pub fn algo(rvalue: ReadSignal<i32>) -> impl IntoView {
+pub fn algo(store_counter: Store<ControlledCounter>) -> impl IntoView {
     Show(ShowProps {
-        when: move || rvalue.get() > 5,
+        when: move || store_counter.value().get() > 5,
         fallback: (move || p().child("I will appear if value is 5 or lower")).into(),
         children: ToChildren::to_children(|| p().child("If value ever goes above 5, the only way to get below 5 again is by resseting")),
     })
@@ -50,13 +57,12 @@ pub fn algo(rvalue: ReadSignal<i32>) -> impl IntoView {
 
 
 pub fn app() -> impl IntoView {
-    let (count, set_count) = signal(0i32);
-    let (lock, set_lock) = signal(false);
+    let store_counter = Store::new(ControlledCounter::default());
 
     view! {
-        { counter(count, set_count, lock, set_lock, 0, 1) }
+        { counter(store_counter, 1) }
         <br/>
-        { algo(count) }
+        { algo(store_counter) }
     }
 }
 
