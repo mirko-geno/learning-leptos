600ba885431def719b661040dc7a63209c316d6b
mirko-geno <mirkogenovesi24@gmail.com>
2026-06-12 02:00:08 -0300

    suboptimal multiple states handling

--- dist/leptos-tutorial-5b6a43bd7ec6b8c3.js
+++ dist/leptos-tutorial-5b6a43bd7ec6b8c3.js
@@ -171,6 +171,10 @@ function __wbg_get_imports() {
             const ret = arg0.cancelBubble;
             return ret;
         },
+        __wbg_cloneNode_49e008a1a100ed8c: function() { return handleError(function (arg0) {
+            const ret = arg0.cloneNode();
+            return ret;
+        }, arguments); },
         __wbg_close_4859304bbf0f8208: function() { return handleError(function (arg0) {
             arg0.close();
         }, arguments); },
@@ -179,6 +183,10 @@ function __wbg_get_imports() {
         }, arguments); },
         __wbg_composedPath_e849604652cb9751: function(arg0) {
             const ret = arg0.composedPath();
+            return ret;
+        },
+        __wbg_createComment_fee3099b63c574fb: function(arg0, arg1, arg2) {
+            const ret = arg0.createComment(getStringFromWasm0(arg1, arg2));
             return ret;
         },
         __wbg_createElementNS_2c964c61db671f5d: function() { return handleError(function (arg0, arg1, arg2, arg3, arg4) {
@@ -220,6 +228,16 @@ function __wbg_get_imports() {
             const ret = arg0.insertBefore(arg1, arg2);
             return ret;
         }, arguments); },
+        __wbg_instanceof_Element_73566cb5986eac3a: function(arg0) {
+            let result;
+            try {
+                result = arg0 instanceof Element;
+            } catch (_) {
+                result = false;
+            }
+            const ret = result;
+            return ret;
+        },
         __wbg_instanceof_ShadowRoot_cfeb6d99e38f5b80: function(arg0) {
             let result;
             try {
@@ -243,6 +261,9 @@ function __wbg_get_imports() {
         __wbg_length_4a591ecaa01354d9: function(arg0) {
             const ret = arg0.length;
             return ret;
+        },
+        __wbg_log_cf2e968649f3384e: function(arg0) {
+            console.log(arg0);
         },
         __wbg_new_50bb5ebeecef71a8: function(arg0, arg1) {
             const ret = new Error(getStringFromWasm0(arg0, arg1));
@@ -284,6 +305,12 @@ function __wbg_get_imports() {
         __wbg_removeEventListener_5f35962e6c0b2ddc: function() { return handleError(function (arg0, arg1, arg2, arg3) {
             arg0.removeEventListener(getStringFromWasm0(arg1, arg2), arg3);
         }, arguments); },
+        __wbg_remove_e74641df7d14f173: function(arg0) {
+            arg0.remove();
+        },
+        __wbg_remove_f3fdf26d49490e53: function(arg0) {
+            arg0.remove();
+        },
         __wbg_resolve_25a7e548d5881dca: function(arg0) {
             const ret = Promise.resolve(arg0);
             return ret;
@@ -339,6 +366,13 @@ function __wbg_get_imports() {
             const ret = typeof window === 'undefined' ? null : window;
             return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
         },
+        __wbg_tagName_45bb7a95d4c5377b: function(arg0, arg1) {
+            const ret = arg1.tagName;
+            const ptr1 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+            const len1 = WASM_VECTOR_LEN;
+            getDataViewMemory0().setInt32(arg0 + 4 * 1, len1, true);
+            getDataViewMemory0().setInt32(arg0 + 4 * 0, ptr1, true);
+        },
         __wbg_target_baf3e983dceee053: function(arg0) {
             const ret = arg0.target;
             return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
@@ -358,17 +392,17 @@ function __wbg_get_imports() {
             console.warn(arg0, arg1, arg2);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 122, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h7b3693b488d542aa);
-            return ret;
-        },
-        __wbindgen_cast_0000000000000002: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 51, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 104, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h0a7454e584d83a84);
             return ret;
         },
+        __wbindgen_cast_0000000000000002: function(arg0, arg1) {
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 185, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h7b3693b488d542aa);
+            return ret;
+        },
         __wbindgen_cast_0000000000000003: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("Event")], shim_idx: 52, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("Event")], shim_idx: 105, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__he17af71671e611f5);
             return ret;
         },
--- src/bin/rusty_sintax.rs
+++ src/bin/rusty_sintax.rs
@@ -1,30 +1,52 @@
 use leptos::{
     ev,
-    html::{div, button, span},
+    html::{div, button, span, p},
     prelude::*,
 };
 
-pub fn counter(initial_value: i32, step: i32) -> impl IntoView {
-    let (count, set_count) = signal(initial_value);
+pub fn counter(rsignal: ReadSignal<i32>, wsignal: WriteSignal<i32>, initial_value: i32, step: i32) -> impl IntoView {
+    wsignal.set(initial_value);
+
     div().child((
         button()
-            .on(ev::click, move |_| set_count.set(0))
+            .on(ev::click, move |_| wsignal.set(0))
             .child("Clear"),
 
         button()
-            .on(ev::click, move |_| *set_count.write() -= step)
+            .on(ev::click, move |_| *wsignal.write() -= step)
             .child("-1"),
 
         button()
-            .on(ev::click, move |_| *set_count.write() += step)
+            .on(ev::click, move |_| *wsignal.write() += step)
             .child("+1"),
         
-        span().child(("Value: ", move || count.get(), "!")),
+        span().child(("Value: ", move || rsignal.get(), "!")),
     ))
 }
 
+pub fn algo(rvalue: ReadSignal<i32>, wvalue: WriteSignal<i32>) -> impl IntoView {
+    Show(ShowProps {
+        when: move || rvalue.get() > 5,
+        fallback: (
+            move || {
+                leptos::logging::log!("fallback");
+                wvalue.update(|value| if *value != 0 { *value += 2 });
+                p().child("I will appear if value is 5 or lower")
+            }
+        ).into(),
+        children: ToChildren::to_children(|| p().child("If value ever goes above 5, the only way to get below 5 again is by resseting")),
+    })
+}
+
+
 pub fn app() -> impl IntoView {
-    counter(0, 1)
+    let (count, set_count) = signal(0i32);
+
+    view! {
+        { counter(count, set_count, 0, 1) }
+        <br/>
+        { algo(count, set_count) }
+    }
 }
 
 pub fn main() {
